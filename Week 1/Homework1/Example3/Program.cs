f3e3f3bfee599fdff99a9b82e501d7f49725ed05
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-06 19:55:35 +0300

    Add Example 3

---
+++ Week 1/Homework1/Example3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Do-While ve While döngüsü nedir? Bununla ilgili örnek yapınız.
+
+int counter = 6;
+
+while (counter <= 5)
+{
+    Console.WriteLine($"While loop: counter {counter}");
+    counter++;
+}
+
+int counter1 = 6;
+do
+{
+    Console.WriteLine($"Do - While loop: Counter: {counter1}");
+    counter1++;
+} while (counter1 <= 5);
