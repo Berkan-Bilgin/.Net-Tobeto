acade8e2fea2d88c818b1787738abd62edd24a1d
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-06 19:55:41 +0300

    Add Example 4

---
+++ Week 1/Homework1/Example4/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Kullanıcının 1 ile 10 arasında rastgele bir sayıyı tahmin etmesini isteyen 
+ve doğru tahmin edene kadar devam eden bir program yazınız.*/
+
+Random random = new Random();
+
+int randomNumber = random.Next(1, 11);
+
+Console.WriteLine("Guess the number between 1 and 10");
+
+int guess = 0;
+
+int counter = 0;
+
+
+do
+{
+    Console.Write("Your Guess: ...");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out guess))
+    {
+        Console.WriteLine("Please enter a valid number ");
+        continue;
+    }
+
+    if (guess < randomNumber)
+    {
+        Console.WriteLine("Increase the guess");
+    }
+    else if (guess > randomNumber)
+    {
+        Console.WriteLine("Decrease the guess");
+    }
+    else
+    {
+        Console.WriteLine("Congratulations!...");
+    }
+    counter++;
+} while (guess != randomNumber);
+
+Console.WriteLine($"Random number: {randomNumber}, Your Guess: {guess}");
+Console.WriteLine($"You guessed it after {counter} tries");
