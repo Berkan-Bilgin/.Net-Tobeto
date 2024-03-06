1c9fcea28bc6ac88b4965bde6eb76a9d7f888b24
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-06 19:55:20 +0300

    add Example 1

---
+++ Week 1/Homework1/Example1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Switch case ile müşteriden alınan sipariş numarasına göre hangi ürünün sipariş 
+//edildiğini belirleyen algoritma yazınız.Input için Scanner kullanınız.
+
+
+Console.WriteLine("Order number: ");
+string orderNumber = Console.ReadLine();
+
+switch (orderNumber)
+{
+    case "0":
+        Console.WriteLine("Your Order: Book");
+        break;
+    case "1":
+        Console.WriteLine("Your Order: Pen");
+        break;
+    case "2":
+        Console.WriteLine("Your Order: Notebook");
+        break;
+    case "3":
+        Console.WriteLine("Your Order: Eraser");
+        break;
+    default:
+        Console.WriteLine("This product is not in stock.");
+        break;
+}
