7a320d0b4d10c3b52638b29a3e0ffbf10bd5dffd
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-03-19 13:28:15 +0300

    Add InterfaceAbstractClass diff

---
+++ Week2/OOP/Homework-OOP2/InterfaceAbstractClassDiff/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Abstract classlar içinde hem metod imzası hem de metod tanımı bulundurabilir.
+
+//Interfacelerde sadece metod imzası bulunmalıdır. .Net 8 ile gelen değişikliklerle beraber interfacelerde de artık metod tanımı bulunabiliyor.
+//Bu durum tavsiye edilmez. Legacy projelere kolaylık sağlansın diye gelmiş bir özellik.
+
+
+//Abstract class lar abstract keywordunu kullanır.
+//Interface ler interface keywordunu kullanır.
+
+//Abstract classlar çoklu kalıtımı desteklemez.
+//Interfaceler ile çoklu kalıtım sağlayabiliriz. Bir class birden fazla interface'e sahip olabilir.
+
+//Abstract classlar  constructor bulundurabilir.
+//Interfacelerde constructor olmaz.
+
+//Interface  bir objenin aynı isime sahip farklı işi yapan metodlarda kullanıma uygundur.
+//Abstract classlar farklı objelerin aynı metodu aynı şekilde uygulandığı durumlarda kullanıma uygundur.
+
